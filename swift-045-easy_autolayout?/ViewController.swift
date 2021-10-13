d407590293658c97ec5f1ee16ef96da2840e7a26
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-10-13 10:37:33 -0300

    Initial Commit

---
+++ swift-045-easy_autolayout?/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-045-easy_autolayout?
+//
+//  Created by Luiz Carlos da Silva Araujo on 13/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
